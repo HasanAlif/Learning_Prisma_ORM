4e4f7205b98f80a9cac31d4ac461a05a23e6c2e0
Mehedi Hasan Alif <alifalmehedihasan@gmail.com>
2026-01-18 17:16:50 +0600

    Complete Crud Operation With Prisma

--- crud.ts
+++ crud.ts
@@ -1,6 +1,7 @@
 import { prisma } from "./lib/prisma";
 
 async function run() {
+  //   Create Operations
   const createUser = await prisma.user.create({
     data: {
       name: "Jane Doe",
@@ -9,6 +10,7 @@ async function run() {
   });
   console.log("User created:", createUser);
 
+  //   Create Post and Profile for the User
   const createPost = await prisma.post.create({
     data: {
       title: "Hello World",
@@ -18,6 +20,7 @@ async function run() {
   });
   console.log("Post created:", createPost);
 
+  // Create Profile for the User
   const createProfile = await prisma.profile.create({
     data: {
       bio: "Software developer & Tester.",
@@ -26,6 +29,7 @@ async function run() {
   });
   console.log("Profile created:", createProfile);
 
+  // Read Operations
   const includeUsers = await prisma.user.findMany({
     include: {
       posts: true,
@@ -46,6 +50,70 @@ async function run() {
   });
   console.log("All Users: ");
   console.dir(selectUsers, { depth: Infinity });
+
+  // Update Operations
+  const updateProfile = await prisma.profile.update({
+    where: { userId: 2 },
+    data: {
+      bio: "Web developer and Frontend Engineer",
+      dateOfBirth: new Date("1990-05-15"),
+    },
+    select: {
+      id: true,
+      bio: true,
+      user: {
+        select: {
+          name: true,
+          email: true,
+        },
+      },
+    },
+  });
+  console.log("Profile updated:", updateProfile);
+
+  //   GetUserDataById
+  const getUserDataById = await prisma.user.findUnique({
+    where: { id: 2 },
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+  console.log("Get User Data By Id:", getUserDataById);
+
+  // Delete Operations
+  // const deleteUser = await prisma.user.delete({
+  //   where: { id: 4 },
+  // });
+  // console.log("User deleted Successfully:", deleteUser);
+
+  const deleteUser = async () => {
+    await prisma.post.deleteMany({
+      where: { authorId: 2 },
+    });
+    await prisma.profile.delete({
+      where: { userId: 2 },
+    });
+    await prisma.user.delete({
+      where: { id: 2 },
+    });
+  };
+  console.log("User deleted Successfully");
+
+  // Upsert Operation
+  const upsertUser = await prisma.user.upsert({
+    where: {
+      email: "jkr@gmail.com",
+    },
+    update: {
+      name: "John Kennedy 2",
+    },
+    create: {
+      name: "John Kennedy 3",
+      email: "jkr@gmail.com",
+    },
+  });
+  console.log("User upserted successfully:", upsertUser);
 }
 
 run()
